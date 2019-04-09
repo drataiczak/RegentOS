c3e609fba0ed698cb6e39269ca6b2a4da5391bba
Devin Rataiczak <rataiczak.devin@gmail.com>
2019-04-09 10:23:01 -0400

    Formatting

--- radon/arch/x86/lib/io.asm
+++ radon/arch/x86/lib/io.asm
@@ -2,9 +2,13 @@ global outb
 
 section .text
 
+;--------------------------------------------------------------------------------------------------
+; Outputs a single byte of data to the given port
+;
 ; void outb(unsigned short port, unsigned char data)
+;--------------------------------------------------------------------------------------------------
 outb:
-    mov al, [esp + 8]   ; argv[1]
-    mov dx, [esp + 4]   ; argv[0]
+    mov al, [esp + 8]   ; Byte to output
+    mov dx, [esp + 4]   ; Address of the I/O port
     out dx, al
     ret
